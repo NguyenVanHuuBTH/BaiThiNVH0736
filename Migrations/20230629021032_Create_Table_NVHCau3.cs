3553456dae11e8763b3081b8bd970f3400196e50
example <nguyenhuuvqht@gmail.com>
2023-06-29 09:12:44 +0700

    NVhCau3

---
+++ Migrations/20230629021032_Create_Table_NVHCau3.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaiThiNVH.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_Table_NVHCau3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NVHCau3",
+                columns: table => new
+                {
+                    Ten = table.Column<string>(type: "TEXT", nullable: false),
+                    Lop = table.Column<string>(type: "TEXT", nullable: true),
+                    DiaChi = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NVHCau3", x => x.Ten);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NVHCau3");
+        }
+    }
+}
